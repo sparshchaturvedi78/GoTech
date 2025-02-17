931984961c5cf87365049f34a4df4007baa38d2e
Sparsh Chaturvedi <133252911+sparshchaturvedi78@users.noreply.github.com>
2025-02-18 02:00:20 +0530

    Update About.jsx

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
                         <p>Driving Innovation in Online Education for a</p>
                         <HighlightText text={'Brighter Future'} />
                         <p className="mx-auto mt-3 text-center text-base font-medium text-richblack-300 lg:w-[95%]">
-                            Studynotion is at the forefront of driving innovation in online education. We're passionate about creating a brighter future by offering cutting-edge courses, leveraging emerging technologies, and nurturing a vibrant learning community.
+                            GoTech is at the forefront of driving innovation in online education. We're passionate about creating a brighter future by offering cutting-edge courses, leveraging emerging technologies, and nurturing a vibrant learning community.
                         </p>
                     </div>
                     <div className="sm:h-[70px] lg:h-[150px]"></div>
