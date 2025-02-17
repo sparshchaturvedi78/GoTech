7e90af53acc323af2dfb5613587b4ffab784ecee
Sparsh Chaturvedi <133252911+sparshchaturvedi78@users.noreply.github.com>
2025-02-17 09:30:16 +0530

    Update Catalog.jsx

--- Frontend/src/pages/Catalog.jsx
+++ Frontend/src/pages/Catalog.jsx
@@ -51,10 +51,10 @@ const Catalog = () => {
     }
 
     return (
-        <div className={`relative w-full bg-richblack-900 mt-10`}>
+        <div className={`relative w-full bg-richblack-900`}>
             <div className='mx-auto box-content px-4 lg:w-[1260px] 2xl:relative '>
                 {/* Hero Section */}
-                <div className=" box-content bg-richblack-800 px-4">
+                <div className=" box-content bg-richblack-800 px-4 py-4">
                     <div className="mx-auto flex min-h-[260px] max-w-maxContentTab flex-col justify-center gap-4 lg:max-w-maxContent ">
                         <p className="text-sm text-richblack-300">
                             {'Home / Catalog / '}
