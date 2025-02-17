040c9e312a02c97a17a7ccacd5c85246851d8b69
Sparsh Chaturvedi <133252911+sparshchaturvedi78@users.noreply.github.com>
2025-02-17 08:22:45 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   port: 3000, // Replace with your desired port number
-  // },
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase this value (in KB) to 1MB
+  },
 });
