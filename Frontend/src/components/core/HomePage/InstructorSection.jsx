2805a5f9a1382ac450e6b6f0a6adc57aef63d738
Sparsh Chaturvedi <133252911+sparshchaturvedi78@users.noreply.github.com>
2025-02-18 01:57:04 +0530

    Update InstructorSection.jsx

--- Frontend/src/components/core/HomePage/InstructorSection.jsx
+++ Frontend/src/components/core/HomePage/InstructorSection.jsx
@@ -21,7 +21,7 @@ const InstructorSection = () => {
                     </div>
                     <p className="font-medium text-[16px] text-justify w-[90%] text-richblack-300">
                         Instructors from around the world teach millions of students on
-                        StudyNotion. We provide the tools and skills to teach what you
+                        GoTech. We provide the tools and skills to teach what you
                         love.
                     </p>
                     <div className="w-fit">
