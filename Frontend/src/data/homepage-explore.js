c727c746d951afb70bb41fa8007c4de59278a622
Sparsh Chaturvedi <133252911+sparshchaturvedi78@users.noreply.github.com>
2025-02-17 22:11:55 +0530

    Update homepage-explore.js

--- Frontend/src/data/homepage-explore.js
+++ Frontend/src/data/homepage-explore.js
@@ -6,19 +6,19 @@ export const HomePageExplore = [
                 heading : "Learn HTML",
                 description : "This course introduces the basic concepts of HTML, focusing on creating and structuring web pages, adding text, links, and images, and understanding fundamental HTML tags.",
                 level : 'Beginner',
-                lessionNumber : 6
+                lessonNumber : 6
             },
             {
                 heading : "Learn CSS",
                 description : "This course covers CSS fundamentals, including how to style HTML elements, create layouts, and use CSS for color, fonts, positioning, and more.",
                 level : 'Beginner',
-                lessionNumber : 6
+                lessonNumber : 6
             },
             {
                 heading : "Responsive Web design",
                 description : "In this course, you'll learn how to make websites responsive, ensuring that your web pages look great on all screen sizes and devices using CSS media queries.",
                 level : 'Beginner',
-                lessionNumber : 6
+                lessonNumber : 6
             },
         ]
     },
@@ -29,19 +29,19 @@ export const HomePageExplore = [
                 heading : "HTML",
                 description : "A beginner-friendly course that covers the essentials of HTML, including how to structure web pages, add text, create links, insert images, and more.",
                 level : 'Beginner',
-                lessionNumber : 6
+                lessonNumber : 6
             },
             {
                 heading : "CSS",
                 description : "This course explores the foundational aspects of CSS, including styling web pages, working with colors, fonts, and layout techniques to create visually appealing websites.",
                 level : 'Beginner',
-                lessionNumber : 6
+                lessonNumber : 6
             },
             {
                 heading : "Responsive ",
                 description : "Learn how to make your websites responsive in this course, with practical tips on creating flexible layouts that adapt to different screen sizes and devices.",
                 level : 'Beginner',
-                lessionNumber : 6
+                lessonNumber : 6
             },
         ]
     },
@@ -52,19 +52,19 @@ export const HomePageExplore = [
                 heading : "Java",
                 description : "In this course, you'll dive into the world of Java programming, starting with the basics of syntax, variables, data types, loops, and conditional statements.",
                 level : 'Beginner',
-                lessionNumber : 6
+                lessonNumber : 6
             },
             {
                 heading : "Python",
                 description : "Python for beginners: Learn the essentials of Python programming, including working with variables, functions, data structures, and object-oriented principles.",
                 level : 'Beginner',
-                lessionNumber : 6
+                lessonNumber : 6
             },
             {
                 heading : "SCSS",
                 description : "This course will help you master SCSS (Sassy CSS), an extension of CSS that allows for variables, nesting, and more powerful styling capabilities.",
                 level : 'Beginner',
-                lessionNumber : 6
+                lessonNumber : 6
             },
         ]
     },
