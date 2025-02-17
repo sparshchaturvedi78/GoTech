76462fd4eafb57a3765a67f7b8a7ecfa993c7ef5
Sparsh Chaturvedi <133252911+sparshchaturvedi78@users.noreply.github.com>
2025-02-17 21:29:16 +0530

    Update SettingsAPI.js

--- Frontend/src/services/operations/SettingsAPI.js
+++ Frontend/src/services/operations/SettingsAPI.js
@@ -25,10 +25,6 @@ export function updateDisplayPicture(token, formData) {
           Authorization: `Bearer ${token}`,
         }
       )
-      console.log(
-        "UPDATE_DISPLAY_PICTURE_API API RESPONSE............",
-        response
-      )
 
       if (!response.data.success) {
         throw new Error(response.data.message)
@@ -36,7 +32,6 @@ export function updateDisplayPicture(token, formData) {
       toast.success("Display Picture Updated Successfully")
       dispatch(setUser(response.data.data))
     } catch (error) {
-      console.log("UPDATE_DISPLAY_PICTURE_API API ERROR............", error)
       toast.error("Could Not Update Display Picture")
     }
     toast.dismiss(toastId)
@@ -50,7 +45,6 @@ export function updateProfile(token, formData) {
       const response = await apiConnector("PUT", UPDATE_PROFILE_API, formData, {
         Authorization: `Bearer ${token}`,
       })
-      console.log("UPDATE_PROFILE_API API RESPONSE............", response)
 
       if (!response.data.success) {
         throw new Error(response.data.message)
@@ -64,7 +58,6 @@ export function updateProfile(token, formData) {
       
       toast.success("Profile Updated Successfully")
     } catch (error) {
-      console.log("UPDATE_PROFILE_API API ERROR............", error)
       toast.error("Could Not Update Profile")
     }
     toast.dismiss(toastId)
@@ -77,7 +70,6 @@ export async function changePassword(token, formData) {
     const response = await apiConnector("POST", CHANGE_PASSWORD_API, formData, {
       Authorization: `Bearer ${token}`,
     })
-    console.log("CHANGE_PASSWORD_API API RESPONSE............", response)
 
     if (!response.data.success) {
       throw new Error(response.data.message)
@@ -85,7 +77,6 @@ export async function changePassword(token, formData) {
     toast.success("Password Changed Successfully")
 
   } catch (error) {
-    console.log("CHANGE_PASSWORD_API API ERROR............", error)
     toast.error(error.response.data.message)
   }
   toast.dismiss(toastId)
@@ -98,7 +89,6 @@ export function deleteProfile(token, navigate) {
       const response = await apiConnector("DELETE", DELETE_PROFILE_API, null, {
         Authorization: `Bearer ${token}`,
       })
-      console.log("DELETE_PROFILE_API API RESPONSE............", response)
 
       if (!response.data.success) {
         throw new Error(response.data.message)
@@ -106,7 +96,6 @@ export function deleteProfile(token, navigate) {
       toast.success("Profile Deleted Successfully")
       dispatch(logout(navigate))
     } catch (error) {
-      console.log("DELETE_PROFILE_API API ERROR............", error)
       toast.error("Could Not Delete Profile")
     }
     toast.dismiss(toastId)
