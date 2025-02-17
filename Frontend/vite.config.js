60c70590dd4705787683495958cbc20dc7a7e527
Sparsh Chaturvedi <133252911+sparshchaturvedi78@users.noreply.github.com>
2025-02-17 08:28:51 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1000, // Increase this value (in KB) to 1MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split libraries like React into separate chunks
+          if (id.includes('node_modules')) {
+            const parts = id.split('node_modules/');
+            const name = parts[parts.length - 1].split('/')[0]; // Extract the first package name
+            return name; // This creates a chunk per package (e.g., react.js, lodash.js)
+          }
+        },
+      },
+    },
   },
 });
