f5dfc6cf70ec00555033a3b78210d7234de79c32
Sparsh Chaturvedi <133252911+sparshchaturvedi78@users.noreply.github.com>
2025-02-17 06:42:03 +0530

    Update Payments.js

--- Backend/Routes/Payments.js
+++ Backend/Routes/Payments.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router()
 
 const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../Controller/payments")
-const { auth, isInstructor, isStudent, isAdmin } = require("../Middlewares/auth")
+const { auth, isInstructor, isStudent, isAdmin } = require("../Middlewares/Auth")
 router.post("/capturePayment", auth, isStudent, capturePayment)
 router.post("/verifyPayment",auth, isStudent, verifyPayment)
 router.post("/sendPaymentSuccessEmail", auth, isStudent, sendPaymentSuccessEmail);
