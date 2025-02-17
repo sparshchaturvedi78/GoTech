305b8c71191478cb546daf0db57a1597db1bb905
Sparsh Chaturvedi <133252911+sparshchaturvedi78@users.noreply.github.com>
2025-02-18 01:56:02 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -216,7 +216,7 @@ const Home = () => {
                         </div>
                         <div className='flex flex-col gap-10 lg:w-[40%] items-start'>
                             <div className='text-[16px]'>
-                                The modern StudyNotion is the dictates its own terms. Today, to
+                                The modern GoTech is the dictates its own terms. Today, to
                                 be a competitive specialist requires more than professional
                                 skills.
                             </div>
